f75a13a988199a788520ad4bdf528d410052f9f8
NicoleSharrock <n.sharrockhayes@gmail.com>
2022-07-16 13:51:14 -0400

    added footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,9 @@ import React from "react";
 function Footer() {
     return (
         <div>
-
+            <footer>
+                <h2>Made with love Nicole Sharrock-Hayes &copy; 2022</h2>
+            </footer>
         </div>
     );
 }
